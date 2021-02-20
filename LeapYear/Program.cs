bc34979a4a5f79826d5c1245ebd42f378d73b86e
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2021-02-20 23:21:26 +0100

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+       static bool is_leap(int myYear)
+        {
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int my_Year;
+            Console.Write("Enter a number please: ");
+            my_Year = Convert.ToInt32(Console.ReadLine());
+            if (is_leap(my_Year))
+            {
+                Console.WriteLine($" {my_Year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($" {my_Year} is not a leap year.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
